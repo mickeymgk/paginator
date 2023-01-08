f25a40335e9256786319acbb25e4f878828da4a7
MickeyMGK <michaellina999@gmail.com>
2023-01-08 16:51:48 +0300

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Paginator from './Paginator'
 function App() {
   return (
     <div className="App">
-      <Paginator page="1" total="100" />
+      <Paginator page={1} total={100} />
     </div>
   )
 }
